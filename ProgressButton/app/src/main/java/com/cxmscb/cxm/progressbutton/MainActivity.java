fde9e35f11ba28c295fe11a551aa3e360b5dedc3
cxmscb <1073216732@qq.com>
2016-09-09 19:53:04 +0800

    code

---
+++ ProgressButton/app/src/main/java/com/cxmscb/cxm/progressbutton/MainActivity.java
@@ -0,0 +1,64 @@
+package com.cxmscb.cxm.progressbutton;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressButton progressButton;
+
+
+    public Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            progressButton.setProgress(msg.arg1);
+        }
+    };
+
+    private int z=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressButton = (ProgressButton) findViewById(R.id.pb);
+        progressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new ProgressThread().start();
+
+
+            }
+        });
+        progressButton.setOnFinishListener(new ProgressButton.OnFinishListener() {
+            @Override
+            public void onFinish() {
+                progressButton.setText("完 成");
+               // progressButton.initState();
+            }
+        });
+
+    }
+
+    public  class ProgressThread extends Thread{
+
+        private int z = 0;
+        @Override
+        public void run() {
+            for(int i = 0;i<10;i++){
+                z+=10;
+                if(i%2==0) try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Message msg = Message.obtain();
+                msg.arg1 = z;
+                handler.sendMessage(msg);
+            }
+        }
+    }
+}
---
+++ ProgressButton/app/src/main/java/com/cxmscb/cxm/progressbutton/ProgressButton.java
@@ -0,0 +1,230 @@
+package com.cxmscb.cxm.progressbutton;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.Button;
+
+/**
+ * Created by cxm on 2016/9/7.
+ */
+public class ProgressButton extends Button {
+
+
+    private int mProgress; //当前进度
+    private int mMaxProgress = 100; //最大进度：默认为100
+    private int mMinProgress = 0;//最小进度：默认为0
+    private GradientDrawable mProgressDrawable;// 加载进度时的进度颜色
+    private GradientDrawable mProgressDrawableBg;// 加载进度时的背景色
+    private StateListDrawable mNormalDrawable; // 按钮在不同状态的颜色效果
+    private boolean isShowProgress;  //是否展示进度
+    private boolean isFinish; //状态是否结束
+    private OnFinishListener onFinishListener; //结束时的监听
+    private float cornerRadius;
+
+
+
+    public ProgressButton(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public ProgressButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public ProgressButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ProgressButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context,AttributeSet attributeSet) {
+
+        mNormalDrawable = new StateListDrawable();
+        mProgressDrawable = (GradientDrawable) getDrawable(
+                R.drawable.rect_progress).mutate();
+        mProgressDrawableBg = (GradientDrawable) getDrawable(
+                R.drawable.rect_progressbg).mutate();
+
+        TypedArray attr =  context.obtainStyledAttributes(attributeSet, R.styleable.progressbutton);
+
+        try {
+
+
+            float defValue = getResources().getDimension(R.dimen.corner_radius);
+            cornerRadius = attr.getDimension(R.styleable.progressbutton_buttonCornerRadius, defValue);
+
+
+            isShowProgress = attr.getBoolean(R.styleable.progressbutton_showProgressNum,true);
+
+
+            mNormalDrawable.addState(new int[]{android.R.attr.state_pressed},
+                    createPressedDrawable(attr));
+            mNormalDrawable.addState(new int[] { }, createNormalDrawable(attr));
+
+
+
+
+            int defaultProgressColor = getResources().getColor(R.color.purple_progress);
+            int progressColor = attr.getColor(R.styleable.progressbutton_progressColor,defaultProgressColor);
+            mProgressDrawable.setColor(progressColor);
+
+
+            int defaultProgressBgColor = getResources().getColor(R.color.progress_bg);
+            int progressBgColor = attr.getColor(R.styleable.progressbutton_progressBgColor,defaultProgressBgColor);
+            mProgressDrawableBg.setColor(progressBgColor);
+
+
+
+        } finally {
+            attr.recycle();
+        }
+
+        isFinish = false;
+
+
+        mProgressDrawable.setCornerRadius(cornerRadius);
+        mProgressDrawableBg.setCornerRadius(cornerRadius);
+        setBackgroundCompat(mNormalDrawable);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        if (mProgress > mMinProgress && mProgress <= mMaxProgress && !isFinish) {
+
+            float scale = (float) getProgress() / (float) mMaxProgress;
+            float indicatorWidth = (float) getMeasuredWidth() * scale;
+
+            mProgressDrawable.setBounds(0, 0, (int) indicatorWidth, getMeasuredHeight());
+
+            mProgressDrawable.draw(canvas);
+
+            if(mProgress==mMaxProgress) {
+                isFinish = true;
+                if(onFinishListener!=null) {
+                    onFinishListener.onFinish();
+                }
+                Log.i("zz",getMeasuredWidth()+"  ");
+            }
+
+        }
+
+        super.onDraw(canvas);
+    }
+
+    public void setProgress(int progress) {
+        mProgress = progress;
+        if(!isFinish){
+
+            if(isShowProgress) setText(mProgress + " %");
+            // 设置背景
+            setBackgroundCompat(mProgressDrawableBg);
+
+        }
+        invalidate();
+    }
+
+
+
+    public int getProgress() {
+        return mProgress;
+    }
+
+    public void setBackgroundCompat(Drawable drawable) {
+        int pL = getPaddingLeft();
+        int pT = getPaddingTop();
+        int pR = getPaddingRight();
+        int pB = getPaddingBottom();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            setBackground(drawable);
+        } else {
+            setBackgroundDrawable(drawable);
+        }
+        setPadding(pL, pT, pR, pB);
+    }
+
+
+
+    private Drawable getDrawable(int id) {
+        return getResources().getDrawable(id);
+    }
+
+
+
+
+    private void initDrawableState() {
+        setBackgroundCompat(mNormalDrawable);
+    }
+
+    public void initState(){
+        initDrawableState();
+        isFinish = false;
+        mProgress = 0;
+    }
+
+
+
+    private Drawable createNormalDrawable( TypedArray attr) {
+
+        GradientDrawable drawableNormal =
+                (GradientDrawable) getDrawable(R.drawable.rect_normal).mutate();// 修改时就不会影响其它drawable对象的状态
+        drawableNormal.setCornerRadius(cornerRadius); // 设置圆角半径
+
+        int defaultNormal =  getResources().getColor(R.color.blue_normal);
+        int colorNormal =  attr.getColor(R.styleable.progressbutton_buttonNormalColor,defaultNormal);
+        drawableNormal.setColor(colorNormal);//设置颜色
+
+        return drawableNormal;
+    }
+
+    private Drawable createPressedDrawable( TypedArray attr) {
+        GradientDrawable drawablePressed =
+                (GradientDrawable) getDrawable(R.drawable.rect_pressed).mutate();// 修改时就不会影响其它drawable对象的状态
+        drawablePressed.setCornerRadius(cornerRadius);// 设置圆角半径
+
+        int defaultPressed = getResources().getColor(R.color.blue_pressed);
+        int colorPressed = attr.getColor(R.styleable.progressbutton_buttonPressedColor,defaultPressed);
+        drawablePressed.setColor(colorPressed);//设置颜色
+
+        return drawablePressed;
+    }
+
+    interface OnFinishListener{
+
+        abstract void onFinish();
+
+    }
+
+    public void setOnFinishListener(OnFinishListener onFinishListener){
+        this.onFinishListener = onFinishListener;
+    }
+
+    public void isShowProgressNum(boolean b){
+        this.isShowProgress = b;
+    }
+
+
+
+}
